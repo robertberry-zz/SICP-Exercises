1ccbea620522c3d37460c907c43ea76b1adcbf78
Robert Berry <robert.berry@liverpool.ac.uk>
2012-08-01 16:34:38 +0100

    Finished most of 125.

--- ex125.lisp
+++ ex125.lisp
@@ -15,11 +15,8 @@
     206
     (gcd 40 (remainder 206 40)))
 
-(if (= 40 0)
-    206
-    (if (= (remainder 206 40) 0)
-        40
-        (gcd (remainder 206 40) (remainder 40 (remainder 206 40)))))
+(if (= (remainder 206 40) 40)
+    
 
 ; etc. it grows infinitely through substitution.
 
