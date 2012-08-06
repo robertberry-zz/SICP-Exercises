4afd5a105bd8ccb7b8f54d1912834d769bb37db3
Robert Berry <rjberry@gmail.com>
2012-08-06 21:45:01 +0100

    Adding work before bed.

--- ex233.lisp
+++ ex233.lisp
@@ -73,5 +73,149 @@
 
 ; Exercise 2.61
 
-(defun adjoin-set (x set)
-  ())
+(defun adjoin-ordered-set (x set)
+  (if (null set)
+      (list x)
+      (let ((fst (car set))
+            (rst (cdr set)))
+        (cond ((= x fst) set)
+              ((> x fst) (cons fst (adjoin-set x rst)))
+              ((< x fst) (list* x fst rst))))))
+
+; Exercise 2.62
+
+(defun union-ordered-set (set1 set2)
+  (cond ((null set1) set2)
+        ((null set2) set1)
+        (t (let ((x1 (car set1))
+                 (x2 (car set2))
+                 (xs1 (cdr set1))
+                 (xs2 (cdr set2)))
+             (cond ((= x1 x2) (cons x1 (union-ordered-set xs1
+                                                          xs2)))
+                   ((< x1 x2) (cons x1 (union-ordered-set xs1 set2)))
+                   ((> x1 x2) (cons x2 (union-ordered-set set1 xs2))))))))
+
+; --
+
+(defun entry (tree)
+  (car tree))
+
+(defun left-branch (tree)
+  (cadr tree))
+
+(defun right-branch (tree)
+  (caddr tree))
+
+(defun make-tree (entry left right)
+  (list entry left right))
+
+(defun element-of-tree-set? (x set)
+  (cond ((null set) nil)
+        ((= x (entry set)) t)
+        ((< x (entry set))
+         (element-of-tree-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-tree-set? x (right-branch set)))))
+
+(defun adjoin-tree-set (x set)
+  (cond ((null set) (make-tree x '() '()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-tree-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-tree-set x (right-branch set))))))
+
+
+; Exercise 2.63
+
+(defun tree->list-1 (tree)
+  (if (null tree)
+      '()
+      (append (tree->list-1 (left-branch tree))
+              (cons (entry tree)
+                    (tree->list-1 (right-branch tree))))))
+
+(defun tree->list-2 (tree)
+  (labels ((copy-to-list (tree result-list)
+             (if (null tree)
+                 result-list
+                 (copy-to-list (left-branch tree)
+                               (cons (entry tree)
+                                     (copy-to-list (right-branch tree)
+                                                   result-list))))))
+    (copy-to-list tree '())))
+
+; a) I'm guessing this is a trick question, as, as far as I can tell, they do.
+;    If you try it on any of the tree examples, you get (1 3 5 7 9 11)
+
+(defparameter +tree-example-1+ (make-tree 7
+                                          (make-tree 3
+                                                     (make-tree 1
+                                                                nil
+                                                                nil)
+                                                     (make-tree 5
+                                                                nil
+                                                                nil))
+                                          (make-tree 9 nil (make-tree 11
+                                                                      nil
+                                                                      nil))))
+
+(defparameter +tree-example-2+ (make-tree 3
+                                          (make-tree 1
+                                                     nil
+                                                     nil)
+                                          (make-tree 7
+                                                     (make-tree 5
+                                                                nil
+                                                                nil)
+                                                     (make-tree 9
+                                                                nil
+                                                                (make-tree 11
+                                                                           nil
+                                                                           nil)))))
+
+(defparameter +tree-example-3+ (make-tree 5
+                                          (make-tree 3
+                                                     (make-tree 1
+                                                                nil
+                                                                nil)
+                                                     nil)
+                                          (make-tree 9
+                                                     (make-tree 7 nil nil)
+                                                     (make-tree 11 nil nil))))
+
+; b) No - a is slower because of the append operation, which is O(n). As for
+;    each layer n is halved, and there are log n layers, it will take O(n log
+;    n) roughly ... I think.
+
+; Exercise 2.64
+
+(defun list->tree (elements)
+  (car (partial-tree elements (length elements))))
+
+(defun quotient (n m)
+  (floor (/ n m)))
+
+(defun partial-tree (elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let ((left-size (quotient (- n 1) 2)))
+        (let ((left-result (partial-tree elts left-size)))
+          (let ((left-tree (car left-result))
+                (non-left-elts (cdr left-result))
+                (right-size (- n (+ left-size 1))))
+            (let ((this-entry (car non-left-elts))
+                  (right-result (partial-tree (cdr non-left-elts)
+                                              right-size)))
+              (let ((right-tree (car right-result))
+                    (remaining-elts (cdr right-result)))
+                (cons (make-tree this-entry left-tree right-tree)
+                      remaining-elts))))))))
+
+; a) oh god
+
