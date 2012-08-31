d16e8c640a0c1d3d0dba84ee49d92c5f191f7a3b
Robert Berry <robert.berry@liverpool.ac.uk>
2012-08-31 14:54:16 +0100

    Finished 4-1-5.

---
+++ ex415.scm
@@ -0,0 +1,10 @@
+(load "ex414.scm")
+
+; Exercise 4.1.5
+
+; If 'try' halts when given 'try', then 'try' by its own definition does not
+; halt, it runs forever.
+
+; If 'try' does not halt when given 'try', then by its own definition it does
+; halt.
+
